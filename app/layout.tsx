37ab7e6ce96243224e227b8a999dcf529ed4e108
Guilherme Crozariol <guicrozariol@gmail.com>
2026-04-29 20:06:51 -0400

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -26,6 +27,7 @@ export default function RootLayout({
           <Background />
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
